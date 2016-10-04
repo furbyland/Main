1c95a6b66388d8abc6f45231de6322befed9722c
furbyland <siirmann@gmail.com>
2016-10-04 10:34:01 +0300

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-        System.out.println ("Hello JAVA");
- 	System.out.println ("Ver 1.0");
-    }
-}
